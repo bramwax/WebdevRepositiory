baa820e1badff61c2210f65d9613f74fcdc604c2
bramwax <andy_bramwell@hotmail.com>
2025-02-20 17:25:20 +0100

    Fixed click problem

--- farmyard/script.js
+++ farmyard/script.js
@@ -1,10 +1,12 @@
 function clicked() {
     console.log("image clicked!!");
 
-    if (clicks == 0) {
+    if (clicks == -1) {
+        sounds[currentIndex].play();
+        clicks = 0;
+    } else if (clicks == 0) {
         txt.innerHTML = animals[currentIndex];
         clicks++;
-
     } else {
 
         if (currentIndex == animals.length - 1) {
@@ -29,13 +31,12 @@ const pig = new Audio('assets/sounds/pig.wav');
 const sheep = new Audio('assets/sounds/sheep.wav');
 let sounds = [chicken, cow, donkey, goat, pig, sheep]
 
-let clicks = 0;
+let clicks = -1;
 let animals = ["Chicken","Cow","Donkey","Goat","Pig","Sheep"];
 let currentIndex = Math.floor(Math.random() * animals.length);
 
 let img = document.getElementById("image");
 let txt = document.getElementById("text");
 
-sounds[currentIndex].play();
 url = "url('assets/images/" + animals[currentIndex] + ".png')";
 img.style.backgroundImage = url;

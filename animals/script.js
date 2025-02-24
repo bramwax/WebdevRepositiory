2deb921c0cf0bff92b1afe88ee207c8d10eaef6f
bramwax <andy_bramwell@hotmail.com>
2025-02-24 10:16:15 +0100

    complete redo

--- animals/script.js
+++ animals/script.js
@@ -2,39 +2,34 @@ function clicked() {
     console.log("image clicked!!");
 
     if (clicks == -1) {
-        url = "url('assets/images/" + animals[currentIndex] + ".png')";
+        url = "url('assets/images/" + animals[currentIndex] + ".svg')";
         img.style.backgroundImage = url;
-        sounds[currentIndex].play();
+
         clicks = 0;
     } else if (clicks == 0) {
         txt.innerHTML = animals[currentIndex];
         clicks++;
     } else {
-
         if (currentIndex == animals.length - 1) {
             currentIndex = 0;
         } else {
             currentIndex++;
         }
-        sounds[currentIndex].play();
         txt.innerHTML = "";
-        url = "url('assets/images/" + animals[currentIndex] + ".png')";
+        url = "url('assets/images/" + animals[currentIndex] + ".svg')";
         img.style.backgroundImage = url;
         clicks = 0;
 
     }
 }
 
-const chicken = new Audio('assets/sounds/chicken.wav');
-const cow = new Audio('assets/sounds/cow.wav');
-const donkey = new Audio('assets/sounds/donkey.wav');
-const goat = new Audio('assets/sounds/goat.wav');
-const pig = new Audio('assets/sounds/pig.wav');
-const sheep = new Audio('assets/sounds/sheep.wav');
-let sounds = [chicken, cow, donkey, goat, pig, sheep]
-
 let clicks = -1;
-let animals = ["Chicken","Cow","Donkey","Goat","Pig","Sheep"];
+let animals = [
+    "ant","badger","beaver","bison","buffalo","camel","cat","chicken",
+    "cow","duck","monkey","mouse","octopus","orangutan","otter","ox",
+    "parrot","penguin","rabbit","ram","rhinoceros","seal","sheep","skunk",
+    "sloth","snail","snake","tiger","turkey","whale","worm"
+];
 let currentIndex = Math.floor(Math.random() * animals.length);
 
 let img = document.getElementById("image");
